821a4ae0f71298cd2559fc0577b1ae08f449fd1a
cartel360 <34124094+cartel360@users.noreply.github.com>
2019-10-07 22:08:26 +0300

    Add files via upload

---
+++ Bank/welcome.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<unistd.h>
+using namespace std;
+void welcome();
+void bill();
+ /*int main()
+ {  
+    welcome();   
+    getch();
+    return 0;
+  } 
+  */
+  
+  
+  
+  void welcome()
+  {
+    
+  cout<<"\n\n\n\n\n\n\n ";
+  system("color A");
+  cout<<"\t\t\t\t\t\t                                ______                  ______     ______                   _______             "<<endl;
+  cout<<"\t\t\t\t\t\t   \\                       /  |          |            |          |      |   |\\      /|   | "<<endl; 
+  cout<<"\t\t\t\t\t\t    \\                     /   |          |            |          |      |   | \\    / |   |       "<<endl; 
+  cout<<"\t\t\t\t\t\t     \\        /\\        /    |          |            |          |      |   |  \\  /  |   |       "<<endl;
+  cout<<"\t\t\t\t\t\t      \\      /  \\      /     |______    |            |          |      |   |   \\/   |   |______   "<<endl;
+  cout<<"\t\t\t\t\t\t       \\    /    \\    /      |          |            |          |      |   |         |   |       "<<endl;
+  cout<<"\t\t\t\t\t\t        \\  /      \\  /       |          |            |          |      |   |         |   |        "<<endl;
+  cout<<"\t\t\t\t\t\t         \\/        \\/        |_______   |________    |______    |______|   |         |   |________  "<<endl;
+  cout<<endl;
+  Sleep(3);
+  system("color E");
+  cout<<endl<<endl<<endl<<endl<<endl;
+  cout<<"\t\t\t\t\t\t                             ____________      _______     "<<endl;
+  cout<<"\t\t\t\t\t\t                                   |          |       |    "<<endl;
+  cout<<"\t\t\t\t\t\t                                   |          |       |    "<<endl;
+  cout<<"\t\t\t\t\t\t                                   |          |       |    "<<endl;
+  cout<<"\t\t\t\t\t\t                                   |          |       |    "<<endl;
+  cout<<"\t\t\t\t\t\t                                   |          |       |    "<<endl;
+  cout<<"\t\t\t\t\t\t                                   |          |_______|    "<<endl;
+  Sleep(2);
+  system("color B");
+  cout<<endl<<endl<<endl<<endl<<endl;
+  bill();
+  Sleep(2); 
+  system("color BC");
+  Sleep(1);
+  system("color 7");
+  }
+ 
+ void bill()
+ {
+ 
+  cout<<"\t\t\t\t\t\t           ________                        \t   _______             _                                            "<<endl;
+  cout<<"\t\t\t\t\t\t          |        |   |   |       |       \t  |        |          / \\        |\\      |   |    /                         "<<endl;
+  cout<<"\t\t\t\t\t\t          |         |  |   |       |       \t  |         |        /   \\       | \\     |   |   /                            "<<endl;
+  cout<<"\t\t\t\t\t\t          |        /   |   |       |       \t  |        /        /     \\      |  \\    |   |  /                           "<<endl;
+  cout<<"\t\t\t\t\t\t          |--------    |   |       |       \t  |  -----         /-------\\     |   \\   |   | /                                 "<<endl;
+  cout<<"\t\t\t\t\t\t          |        |   |   |       |       \t  |        |      /         \\    |    \\  |   |  \\                             "<<endl;
+  cout<<"\t\t\t\t\t\t          |         |  |   |       |       \t  |         |    /           \\   |     \\ |   |   \\                               "<<endl;
+  cout<<"\t\t\t\t\t\t          |_______ |   |   |______ |______ \t  |_______ |    /             \\  |      \\|   |    \\                                  "<<endl;
+ 
+ }
+  
